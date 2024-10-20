480052ef9b58ce675ae7cb64da119cacaf301172
ernestodiaz <carlos.ernesto.diaz.98@gmail.com>
2024-10-19 23:37:12 -0500

    :tada: create project

---
+++ packages.R
@@ -0,0 +1,13 @@
+# Lista de paquetes necesarios
+install.packages(c(
+  "ggplot2",
+  "dplyr",
+  "readr",
+  "tidyverse"
+))
+
+# Cargar los paquetes instalados
+library(ggplot2)
+library(dplyr)
+library(readr)
+library(tidyverse)
