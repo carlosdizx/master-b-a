f2fca5a5d9d9319ced20d1bcec86aa0553147c80
ernestodiaz <carlos.ernesto.diaz.98@gmail.com>
2024-11-01 15:10:06 -0500

    :heavy_plus_sign: install lattice future.apply caret e1071

--- packages.R
+++ packages.R
@@ -1,11 +1,16 @@
 # Lista de paquetes necesarios
+install.packages("future.apply", type = "binary")
 install.packages(c(
   "ggplot2",
+  "lattice",
+  "future.apply",
   "dplyr",
   "readr",
   "tidyverse",
   "caTools",
   "ROCR",
   "rpart",
-  "rpart.plot"
+  "rpart.plot",
+  "caret",
+  "e1071"
 ))
